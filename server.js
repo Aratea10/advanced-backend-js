9b0c4a96d1f6c0136d26663812b6e7b32528a375
Sara Gallego Méndez <saragm10@hotmail.com>
2026-02-12 23:29:52 +0100

    feat: setup initial server and docker environment

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("server.js");
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express();
+
+try {
+    await mongoose.connect('mongodb://admin:admin123@localhost:27017/db?authSource=admin');
+    console.log('mongodb connected =)');
+
+} catch (error) {
+    console.log("mongodb error: ", error);
+}
+
+app.get('/example', (request, response) => {
+    response.json({
+        info: "first endpoint"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("up & running on port: ", 3000);
+})
